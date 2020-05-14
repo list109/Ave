8109f767c07cfe72594ac7349f97c6629ddbce23
Vladimit Bonadernko <list109@list.ru>
2020-05-14 15:21:11 +0300

    sticky menu feature

--- app/js/libs/menu.js
+++ app/js/libs/menu.js
@@ -4,8 +4,5 @@ function menu(btn, btnClass) {
     btn.onclick = () => {
             btn.classList.toggle(btnClass);
             document.body.classList.toggle(btn.dataset.bodyClass);
-            document.querySelector('.mobile__top').style.right = (btn.classList.contains(btnClass)) ? 
-            scrollLineWidth + 'px'
-            : ''; 
     }
 };
