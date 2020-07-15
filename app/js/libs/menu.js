504f3c5fc50bf765f0005f8c7263ef6841d416dd
Vladimir <git_109@list.ru>
2020-07-15 21:34:39 +0300

    clean the directory for GitHub pages project site version

--- app/js/libs/menu.js
+++
@@ -1,8 +0,0 @@
-let scrollLineWidth = window.innerWidth - document.documentElement.clientWidth;
-
-function menu(btn, btnClass) {
-    btn.onclick = () => {
-            btn.classList.toggle(btnClass);
-            document.body.classList.toggle(btn.dataset.bodyClass);
-    }
-};
